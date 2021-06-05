15c3fab49552e8c34371070e3070b2b6bc829e84
HunterLiddell <liddellh@uwplatt.edu>
2021-06-04 21:17:18 -0500

    Add project files.

---
+++ Bank/ApplicationTier/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bank
+{
+    class Startup
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
